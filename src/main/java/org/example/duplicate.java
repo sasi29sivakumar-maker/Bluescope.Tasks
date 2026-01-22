d34b4a66351847863f71c591f9766837e875d23c
sasi_sk <sasi29sivakumar@gmail.com>
2026-01-22 10:21:23 +0530

    I practiced reverse array HackerRank problems

---
+++ src/main/java/org/example/duplicate.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class duplicate {
+
+        public static void main(String[] args) {
+
+            int a[]= {2,5,5,6,3,9,10,9,11,8};
+
+
+
+            for(int i=a.length-1;i>0;i--) {
+
+                System.out.println(a[i]);
+
+
+            }
+        }
+}
+
