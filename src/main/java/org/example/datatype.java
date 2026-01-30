b285a18a221b7dd9443699a56759e5e59c66598f
sasi_sk <sasi29sivakumar@gmail.com>
2026-01-30 11:40:11 +0530

    I Practiced java datatype

---
+++ src/main/java/org/example/datatype.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class datatype {
+
+        public static void main(String [] args) {
+            int a=10;
+            char c='b';
+            String str="Hello world";
+            System.out.println("a="+a);
+            System.out.println("c="+c);
+            System.out.println("str="+str);
+        }
+    }
+
