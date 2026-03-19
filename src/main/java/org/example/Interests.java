4c78da6c01b9c5c12c545b6bc98b6b30489e003b
sasi_sk <sasi29sivakumar@gmail.com>
2026-03-19 15:09:14 +0530

    Interest

---
+++ src/main/java/org/example/Interests.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Interests {
+
+    public static void main(String[] args) {
+
+        double p = 10000;
+        double r = 5;
+        double t = 2;
+
+        double si = (p * r * t) / 100;
+
+        System.out.println("Interest: " + si);
+    }
+}
